ecef90a6ef04ee62ad13ec0a0874945e84556dd2
spzhang <spzhang@localhost.localdomain>
2015-05-05 23:23:01 -0400

    fix bugs

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # clone vundle
-if [ -d ~/.vim/bundle/Vundle.vim]; then
+if !([ -d ~/.vim/bundle/Vundle.vim ]); then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +VundleInstall +qall
 fi
@@ -9,7 +9,7 @@ fi
 cp .vimrc ~/.vimrc
 
 # change the font 
-if [`uname` = "Darwin"]; then
+if [ `uname` = "Darwin" ]; then
     ex -s +"%s/\"\ set\ guifont=M/set\ guifont=M/g" -cxa ~/.vimrc
 else
     ex -s +"%s/\"\ set\ guifont=u/set\ guifont=u/g" -cxa ~/.vimrc
